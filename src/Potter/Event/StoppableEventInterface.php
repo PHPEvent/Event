2cf0d5be501962925d3dc095a521482237bbae9e
Jay Potter <j@ypotter.ca>
2024-11-03 13:37:30 -0500

    Create StoppableEventInterface.php

---
+++ src/Potter/Event/StoppableEventInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Potter\Event;
+
+use Psr\EventDispatcher\StoppableEventInterface as PsrStoppableEventInterface;
+
+interface StoppableEventInterface extends PsrStoppableEventInterface
+{
+    public function isPropagationStopped(): bool;
+    public function stopPropagation(): void;
+}
